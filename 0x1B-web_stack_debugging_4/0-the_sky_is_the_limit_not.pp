91bf7c57989e4c572502aed34af69d6b812c8cf9
abdelhak <chenafa.abdelhak@gmail.com>
2024-06-26 15:36:15 -0700

    fix ulimit -n

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,18 @@
+# manifest to change ulimit so the nginx can handle appropriate amount of requests
+
+exec { 'update_ulimit':
+  provider => shell,
+  command  => "sudo sed -i 's/^ULIMIT=.*/ULIMIT=-n 4096/' /etc/default/nginx",
+  before   =>  exec['restart_nginx'],
+}
+
+exec { 'restart_nginx':
+  provider => shell,
+  command  => "sudo service nginx restart",
+  subscribe=> Service['nginx'],
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
